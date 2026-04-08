2be485193bdd67babae2acf14635dbbd4c6a8bf5
Gilbertq1 <muhammadgilberto941@gmail.com>
2026-04-09 03:19:43 +0700

    fix: bind fastify to 0.0.0.0 for railway networking

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { env } from "./config/env";
 
 const app = buildApp();
 
-app.listen({ port: env.PORT }, (err, address) => {
+app.listen({ port: env.PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
